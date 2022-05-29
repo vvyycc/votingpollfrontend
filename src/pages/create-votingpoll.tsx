6091c454930f79eb91d9148b51623464e143f996
Victor <victorroldan18@gmail.com>
2022-05-29 23:09:05 +0200

    Update create-votingpoll.tsx

--- src/pages/create-votingpoll.tsx
+++ src/pages/create-votingpoll.tsx
@@ -54,7 +54,7 @@ const CreateVotingPoll = ({isActive}:any) => {
 
     return (
         <>
-           {isActive? <div x-cloak className="w-3/5 mx-auto mt-20 mb-10">
+           {isActive? <div x-cloak="true" className="w-3/5 mx-auto mt-20 mb-10">
                 <Link href="/">
                     <button className='inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'> Home</button>
                 </Link>
